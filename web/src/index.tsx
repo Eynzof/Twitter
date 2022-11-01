93b71d46563ec22a9b60ff3790d977f952eb1bfd
enzo <Eynzof@gmail.com>
2022-11-01 15:33:25 +0800

    auth header to apollo client

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -11,27 +11,27 @@ import Landing from "./components/landing";
 import Users from "./components/user";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { setContext } from "apollo-link-context";
+import { setContext } from "@apollo/client/link/context";
+import Signup from "./pages/signup";
+import Login from "./pages/login";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-const httpLink = new HttpLink({ uri: "http://localhost:4000" });
-
-const authLink = setContext(async (req, { headers }) => {
-  const token = localStorage.getItem("token");
-  console.log("http link", httpLink);
-  return {
-    ...headers,
-    headers: {
-      Authorization: token ? `Bearer ${token}` : null,
-    },
+const authLink = setContext((request, previousContext) => {
+  const result = {
+    headers: { authorization: localStorage.getItem("token") },
   };
+  return result;
+});
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000",
+  credentials: "same-origin",
 });
 
 const link = authLink.concat(httpLink as any);
-
 const client = new ApolloClient({
   link: link as any,
   cache: new InMemoryCache(),
@@ -45,6 +45,14 @@ const router = createBrowserRouter([
   {
     path: "landing",
     element: <Landing />,
+  },
+  {
+    path: "signup",
+    element: <Signup />,
+  },
+  {
+    path: "login",
+    element: <Login />,
   },
 ]);
 
