b8b3a7c8d7566b08eeccc78e7a60c3d7764f7825
enzo <Eynzof@gmail.com>
2022-11-01 21:56:19 +0800

    add image bg

--- web/src/components/landing.tsx
+++ web/src/components/landing.tsx
@@ -6,9 +6,8 @@ import "../styles/utility.css";
 function Landing() {
   return (
     <main style={{ backgroundColor: "#C5C5C5" }}>
-      <div id="image-section" style={{ backgroundColor: "#808080" }}>
+      <div id="image-section">
         <div></div>
-        <svg></svg>
       </div>
       <section id="content-section" style={{ backgroundColor: "#d9d9d9" }}>
         <section id="content-section-wrapper">
