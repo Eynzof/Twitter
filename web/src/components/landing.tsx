df4193ec812210c3434890df0a2c150d5891fa8b
enzo <Eynzof@gmail.com>
2022-11-01 21:36:01 +0800

    add wrapper

--- web/src/components/landing.tsx
+++ web/src/components/landing.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import TwitterLogo from "../styles/assets/TwitterLogo.png";
 import "../styles/landing.css";
-import "../styles/utility.css";
+import "../styles/uti.css";
 function Landing() {
   return (
     <main style={{ backgroundColor: "#C5C5C5" }}>
@@ -11,10 +11,12 @@ function Landing() {
         <svg></svg>
       </div>
       <section id="content-section" style={{ backgroundColor: "#d9d9d9" }}>
-        <svg></svg>
-        <h1>Happening now</h1>
-        <h3>Join Twitter today.</h3>
-        <section></section>
+        <section id="content-section-wrapper">
+          <svg></svg>
+          <h1>Happening now</h1>
+          <h3>Join Twitter today.</h3>
+          <section></section>
+        </section>
       </section>
     </main>
   );
