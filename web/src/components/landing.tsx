a7e51b9d9bf131b7a6e9855a5fad6246b90cbe39
enzo <Eynzof@gmail.com>
2022-11-02 07:20:28 +0800

    add functions

--- web/src/components/landing.tsx
+++ web/src/components/landing.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import twitterlogo from "../styles/assets/twitterlogo.png";
+import applelogo from "../styles/assets/icons8-apple-logo-60.png";
 import "../styles/landing.css";
 import "../styles/utility.css";
 function Landing() {
@@ -18,7 +19,33 @@ function Landing() {
             <h1>Happening now</h1>
           </div>
           <h2 id="heading2">Join Twitter today.</h2>
-          <section></section>
+          <section id="login-section">
+            <a className="signup-g" href="/login-g">
+              <img
+                src="https://img.icons8.com/color/48/000000/google-logo.png"
+                alt="google logo"
+              />
+              Sign up with Google
+            </a>
+            <a className="signup-a" href="/login-a">
+              <img src={ applelogo } alt="apple logo" />
+              Sign up with Apple
+            </a>
+            <div className="or">
+              <hr className="hr1" />
+              <h6> or </h6>
+              <hr className="hr2" />
+            </div>
+            <button className="button1">Sign up with phone or email</button>
+            <p>
+              By signing up, you agree to the
+              <a href=""> Terms of Service</a> and
+              <a href="">Privacy Policy </a>, including
+              <a href="">Cookie Use.</a>
+            </p>
+            <h4>Already have an account?</h4>
+            <button className="button2">Sign in</button>
+          </section>
         </section>
       </section>
     </main>
