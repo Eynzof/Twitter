6b290085a9bcbde3fa4f00e8b36efee03a313bba
enzo <Eynzof@gmail.com>
2022-11-01 15:38:31 +0800

    fix

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 
 const LOGIN_MUTATION = gql`
   mutation login($email: String!, $password: String!) {
-    login(name: $name, email: $email, password: $password) {
+    login(email: $email, password: $password) {
       token
     }
   }
