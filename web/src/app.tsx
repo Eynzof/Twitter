b40d685a882a309465a9f5317cab232bc0aefc94
enzo <Eynzof@gmail.com>
2022-11-02 19:52:47 +0800

    create profile page

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -4,7 +4,8 @@ import Users from "./components/user";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 import IsAuthenticated from "./utils/isAuthenticated";
-import './app.css'
+import "./app.css";
+import Profile from "./pages/profile";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/landing" element={<Landing />} />
         <Route path="/" element={<IsAuthenticated />}>
           <Route path="/" element={<Users />} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
