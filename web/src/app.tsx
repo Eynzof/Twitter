0228c8ab75b18cd82f8dafa338645c2dadb4b83f
enzo <Eynzof@gmail.com>
2022-11-03 14:34:20 +0800

    add 404 page

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -6,15 +6,19 @@ import Signup from "./pages/signup";
 import IsAuthenticated from "./utils/isAuthenticated";
 import "./app.css";
 import Profile from "./pages/profile";
+import CreateProfile from "./components/CreateProfile";
+import NotFound from "./pages/notfound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<NotFound />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="/" element={<IsAuthenticated />}>
           <Route path="/users" element={<Users />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/create-profile" element={<CreateProfile />} />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
